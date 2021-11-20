f10f6a3c3e0cc6d1b8f229c253652f25061ff876
josecarvajalhernandez <jose.carvajal.hernandez@hotmail.com>
2021-11-20 18:26:05 -0300

    agregando funcion core que ejecutara logica de ruleta y optimizando coneccion a puerto mediante variable de entorno

--- ruleta_backend/src/index.ts
+++ ruleta_backend/src/index.ts
@@ -1,7 +1,6 @@
 import app from './app'
 import './database'
 
-
-app.listen(3000,()=>{
-    console.log('server on port 3000')
+app. listen(app.get('port'),()=>{
+    console.log('servidor en puerto: ', app.get('port'));
 })
