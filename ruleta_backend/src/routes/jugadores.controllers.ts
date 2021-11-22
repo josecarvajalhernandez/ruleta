8b12f8bf765126b6c29fa8aa83bb436ecf294056
josecarvajalhernandez <jose.carvajal.hernandez@hotmail.com>
2021-11-22 00:42:42 -0300

    probrando actualizacion por siglo

--- ruleta_backend/src/routes/jugadores.controllers.ts
+++ ruleta_backend/src/routes/jugadores.controllers.ts
@@ -30,12 +30,11 @@ export const getDefault: RequestHandler = async(req, res) => {
      //   const jugadores = await Jugador.find()
         const jugadores = ['algo','otro','probando','alfin esto pinta bien']
         console.log(jugadores);
-     return res.json(jugadores);
+        return res.json(jugadores);
     } catch (error){
         //return  res.json(error);  
         res.json(error); 
     }
-    
     const jugadores = await Jugador.find();
     return res.json(jugadores);
 }
@@ -45,20 +44,21 @@ const intervaloEjecutarRuleta = 180000;
 setInterval(async()=>{
     console.log('esto será la funcion core desde el ctrl:' + Date());
     const listaJugadores =  await Jugador.find();
-    //const arreglo = { "nombre": "jhon-snow", "saldo": "120010" }
-  //  let jugadorUpdated = await Jugador.findByIdAndUpdate("619ae066d0581ce0afb2562d",arreglo);
-    console.log(listaJugadores);
+    //const arreglo = { "nombre": "jhon-snow", "saldo": "708010" }
+    //let jugadorUpdated = await Jugador.findByIdAndUpdate("619ae066d0581ce0afb2562d",arreglo);
+    listaJugadores.forEach(async element => {
+        let jugadorUpdated = await Jugador.findOneAndUpdate({ name: element.nombre}, { saldo: 10000 });
+        console.log(jugadorUpdated);
+        console.log(element.nombre);
+    });
 }, intervaloEjecutarRuleta);
-
 
 export const getJugador: RequestHandler = async(req, res) => {
     //const jugadorFound = await Jugador.findById(req.params.id)
-    const jugadorFound = await Jugador.findOne({_id:req.params.id})
-    
+    const jugadorFound = await Jugador.findOne({_id:req.params.id})    
     if(!jugadorFound){
         return res.status(200).json;
     }
-
     return res.json(jugadorFound);
 }
 
@@ -72,7 +72,6 @@ export const deleteJugador: RequestHandler = async(req, res) => {
 
 export const updateJugador: RequestHandler = async(req, res) => {
     const jugadorUpdated = await Jugador.findByIdAndUpdate(req.params.id, req.body,{ new:true });
-    console.log(req.body);
     if(!jugadorUpdated){
         return res.status(204).json();
     }
